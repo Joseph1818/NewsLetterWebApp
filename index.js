5cbfa78c5f968127142a4fd37b34a4abfd6b78a6
Joseph1818 <ujee@UMBAS-MacBook-Air.local>
2023-07-28 21:35:32 +0200

    Commit 6. Index.ejs and css Working on Js File

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { dirname } from "path";
+import * as path from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+// Static Files
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/images", express.static(__dirname + "public/images"));
+
+//Set views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Get Handler(help to send back the resource that the user want.)
+app.get("", (req, res) => {
+  res.render("index");
+});
+app.get("/confirmationPage", (req, res) => {
+  res.render("confirmationPage");
 });
 
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+});
+
+//Listen on port 3000
 app.listen(3000, () => {
   console.log("Server running on port 3000.");
 });
